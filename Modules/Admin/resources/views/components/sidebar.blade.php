ee8cb75d01a0b6b4755a2c3df4c0d373b2a15063
Aziz Traders <aziz.traders.bgt.developer@gmail.com>
2025-03-16 03:53:23 +0530

    logo added, sidebar updated

--- Modules/Admin/resources/views/components/sidebar.blade.php
+++ Modules/Admin/resources/views/components/sidebar.blade.php
@@ -1,77 +1,35 @@
 <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100"
     style="min-width: 200px;">
-    <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-        <span class="fs-5 d-none d-sm-inline">Admin Panel</span>
+    <a href="{{ route('admin.dashboard.index') }}"
+        class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+        <img src="{{ asset('images/logo.png') }}" alt="Discover Islam" class="img-fluid" style="height: 60px; width:200px;">
     </a>
+
     <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start flex-grow-1"
         id="menu">
         <li class="nav-item">
             <a href="#" class="nav-link align-middle px-0 text-white">
-                <i class="fs-4 bi-house"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span>
             </a>
-        </li>
-        <li>
-            <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
-                <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Dashboard</span>
-            </a>
-            <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
-                <li class="w-100">
-                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Item</span>
-                        1</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Item</span>
-                        2</a>
-                </li>
-            </ul>
-        </li>
-        <li>
-            <a href="#" class="nav-link px-0 align-middle text-white">
-                <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span>
-            </a>
-        </li>
-        <li>
-            <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
-                <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span>
-            </a>
-            <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                <li class="w-100">
-                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Item</span>
-                        1</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Item</span>
-                        2</a>
-                </li>
-            </ul>
         </li>
         <li>
             <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle text-white">
-                <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span>
+                <i class="fas fa-users"></i> <span class="ms-1 d-none d-sm-inline">Users</span>
             </a>
             <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
                 <li class="w-100">
-                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Product</span>
-                        1</a>
+                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Admin</span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Product</span>
-                        2</a>
+                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Verified</span>
+                    </a>
                 </li>
                 <li>
-                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Product</span>
-                        3</a>
-                </li>
-                <li>
-                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">Product</span>
-                        4</a>
+                    <a href="#" class="nav-link px-0 text-white"> <span class="d-none d-sm-inline">General</span>
+                    </a>
                 </li>
             </ul>
-        </li>
-        <li>
-            <a href="#" class="nav-link px-0 align-middle text-white">
-                <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span>
-            </a>
         </li>
     </ul>
 
